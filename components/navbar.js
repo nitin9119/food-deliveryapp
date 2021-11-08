1eba02ae7c011a6f0ce22eea7d77961a4cbdd3ec
nitin9119 <91542487+nitin9119@users.noreply.github.com>
2021-11-08 17:04:23 +0530

    first commit

---
+++ components/navbar.js
@@ -0,0 +1,13 @@
+function navbar(){
+    return `<h3><a href="index.html">Home</a></h3>
+    <div id="option">
+        <h3> <a href="menu.html">MENU</a></h3>
+        <h3> <a href="login.html">Login</a></h3>
+        <h3> <a href="SignUp.html">SignUp</a></h3>
+        <h3> <a href="cart.html">Cart</a></h3>
+    </div>`
+    
+    
+}
+
+export default navbar
